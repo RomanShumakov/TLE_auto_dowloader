a70af43df3dfee894cd2bfff44a8bd2835144115
Роман Шумаков <romeo46100@mail.ru>
2026-07-01 16:13:56 +0300

    optimize output logic

--- data_taker.py
+++ data_taker.py
@@ -39,14 +39,19 @@ def take_data_from_site():
             # query_url = 'https://www.space-track.org/basicspacedata/query/class/satcat/limit/1/format/json'
             # https://www.space-track.org/basicspacedata/query/class/gp/EPOCH/%3Enow-30/orderby/NORAD_CAT_ID,EPOCH/format/3le
             # "https://www.space-track.org/basicspacedata/query/class/gp/EPOCH/>now-30/MEAN_MOTION/>11.25/format/3le"
-            query_url = 'https://www.space-track.org/basicspacedata/query/class/gp/orderby/EPOCH%20desc/format/json'
-            data_response = session.get(query_url)
 
-            if data_response.status_code != 200:
-                print(f"Не удалось получить данные. Код ошибки: {data_response.status_code}")
+            try:
+                query_url = 'https://www.space-track.org/basicspacedata/query/class/gp/orderby/EPOCH%20desc/format/json'
+                data_response = session.get(query_url)
+
+                if data_response.status_code != 200:
+                    print(f"Не удалось получить данные. Код ошибки: {data_response.status_code}")
+                    return None
+                else:
+                    return data_response.json()
+            except Exception as e:
+                print(f"Что-то пошло не так: {e}")
                 return None
-            else:
-                return data_response.json()
 
     except Exception as e:
         print(f"Что-то пошло не так: {e}")
@@ -54,6 +59,11 @@ def take_data_from_site():
 
 
 def data_writer(data):
+
+    if not data:
+        print("Данных нет, записывать нечего.")
+        return None
+
     today = datetime.now().strftime("%Y%m%d")
 
     folderpath = os.path.join("TLE", today)
@@ -61,16 +71,21 @@ def data_writer(data):
     os.makedirs(folderpath, exist_ok=True)
     file_path = os.path.join(folderpath, 'ALL.txt')
 
+    lines_count = 0
     with open(file_path , 'w', encoding='utf-8') as file:
         for entry in data:
             name = entry.get('OBJECT_NAME', 'Unknown')
             tle_line1 = entry.get('TLE_LINE1')
             tle_line2 = entry.get('TLE_LINE2')
+            lines_count += 3
 
             file.write(f"{name}\n{tle_line1}\n{tle_line2}\n")
 
     print(f"Готово! Записано {len(data)} объектов в файл ALL.txt")
-    print(f"Это составляет {len(data) * 3} строк")
+    if lines_count == len(data) * 3:
+        print(f"Проверка целостности пройдена: записано {lines_count} строк.")
+    else:
+        print(f"Ошибка целостности! Ожидалось {len(data) * 3}, но записано {lines_count}.")
 
 
 if __name__ == "__main__":
